c7a78b7201598b263f862a0a27da24fa9dd829a7
Abraham Herrera <f170004@gmail.com>
2024-05-07 01:48:13 -0600

    crear formulario de actualizacion y consumir datos

---
+++ src/app/directory/update-contact/update-contact.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ContactsService } from '../services/contacts.service';
+
+@Component({
+  selector: 'app-update-contact',
+  templateUrl: './update-contact.component.html',
+  styleUrls: ['./update-contact.component.css']
+})
+export class UpdateContactComponent {
+
+  public id!: string;
+  showAddressField: boolean = false;
+  showPhoneField: boolean = false;
+  showEmailField: boolean = false;
+
+  public data!:any;
+
+  constructor(private route: ActivatedRoute,
+    private _contactService:ContactsService,) {}
+
+
+
+  toggleAddressField() {
+    this.showAddressField = !this.showAddressField;
+  }
+
+  togglePhoneField() {
+    this.showPhoneField = !this.showPhoneField;
+  }
+
+  toggleEmail() {
+    this.showEmailField = !this.showEmailField;
+  }
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+
+   this.getContactbyId();
+  }
+
+
+
+  getContactbyId(){
+    this._contactService.getContactbyId(this.id).subscribe({
+      next: ({ data, code, status }) => {
+        if( code === 200 && status == "success" ){ 
+
+          this.data=data
+          console.log(this.data)
+          }          
+      },
+      error: ( error ) => {
+        console.log( error );
+      }
+    })
+  }   
+
+
+
+  onSubmit(form: NgForm) {
+ 
+    Swal.fire({
+      title: "¿Decea crear un nuevo Contacto?",
+      showCancelButton: true,
+      confirmButtonText: "si"
+    }).then((result) => {
+      if (result.isConfirmed) {        
+        console.log(form.value.name)
+
+        this._contactService.addContact(form.value).subscribe(
+          (resp) => {
+            if (resp.status === "success") {
+              Swal.fire("contacto: "+ resp.contact.name +" "+ resp.contact.lastname +" almacenado correctamente!", "", "success");
+              setTimeout(() => {
+                window.location.reload();
+              }, 3000);
+            } else {
+              Swal.fire(resp.message != undefined ? resp.message : resp.errors != undefined && resp.errors.length > 0 ? resp.errors[0] : 'Ocurrió un problema', "", "error");
+            }
+          }              
+        );  
+      }
+    });
+
+  }
+
+}
